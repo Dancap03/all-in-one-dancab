d712b3cf3264a29a79a9b6cecc825ae8b885816e
dancap03 <146107009+Dancap03@users.noreply.github.com>
2026-05-14 12:01:04 +0200

    Create config.ts

---
+++ frontend/src/infrastructure/firebase/config.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+
+// Sustituye estos valores con los que copiaste de tu nueva consola de Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDdds3IQ-S4E59jwP2CSbMJzuJYhseVSIs",
+  authDomain: "allinone-superapp-dancab.firebaseapp.com",
+  projectId: "allinone-superapp-dancab",
+  storageBucket: "allinone-superapp-dancab.firebasestorage.app",
+  messagingSenderId: "117993544584",
+  appId: "1:117993544584:web:47c782b8d120a7457b783d",
+  measurementId: "G-JY534JGLFG"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
